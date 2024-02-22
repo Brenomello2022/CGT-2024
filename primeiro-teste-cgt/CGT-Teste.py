7cb6872111239dc7a9a3b8cc88a519824421bfe6
Breno de Mello <110641736+Brenomello2022@users.noreply.github.com>
2024-02-22 19:03:58 -0300

    adicionando mais testes

--- primeiro-teste-cgt/CGT-Teste.py
+++ primeiro-teste-cgt/CGT-Teste.py
@@ -1,11 +1,11 @@
-#imports
+# Imports.
 
 import sys
 import pygame
 
 pygame.init()
 
-#configurações da tela
+# Configurações da Tela.
 
 largura = 800
 altura = 600
@@ -20,11 +20,15 @@ tamanho_fonte = 50
 fonte = pygame.font.SysFont(None, tamanho_fonte)
 
 texto = fonte.render("Player", True, BRANCO)
+
+# Posicionamento do Texto.
+
 # text_rect = texto.get_rect(center=(largura/2, altura/2)) # Centro
+# text_rect = texto.get_rect(center=(largura/2, 25)) # Parte de Cima
+# text_rect = texto.get_rect(center=(400, 575)) # Parte de Baixo
 
-# text_rect = texto.get_rect(center=(largura/2, 25)) # Topo
-
-text_rect = texto.get_rect(center=(0, altura/2)) # Baixo
+# text_rect = texto.get_rect(center=(730, 300)) # Lado Direito
+# text_rect = texto.get_rect(center=(70, 300)) # Lado Esquerdo
 
 # text_rect = texto.get_rect(center=(55, 25)) # Canto Superior Esquerdo
 # text_rect = texto.get_rect(center=(735, 30)) # Canto Superior Direito 
@@ -32,7 +36,7 @@ text_rect = texto.get_rect(center=(0, altura/2)) # Baixo
 # text_rect = texto.get_rect(center=(740, 570)) # Canto Inferior Direito
 # text_rect = texto.get_rect(center=(55, 570)) # Canto Inferior Esquerdo
 
-#Loop Principal
+# Loop Principal.
 
 while True:
     for event in pygame.event.get():
