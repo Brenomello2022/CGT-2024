6033d9d0c0f6e370f63b432edb7c7a1fe1967dae
Breno de Mello <110641736+Brenomello2022@users.noreply.github.com>
2024-06-19 19:34:29 -0300

    Alrerações

--- unity-engine/codigo-zumbi-versao2/zombie.cs
+++ unity-engine/codigo-zumbi-versao2/zombie.cs
@@ -4,47 +4,57 @@ using UnityEngine;
 
 public class Zombie : MonoBehaviour
 {
-    public float speed = 0.05f;
-    public float rotationSpeed = 100.0f;
-    public Animator animator;
+    // Start is called before the first frame update
+    public float speed = 0.05f;             // Velocidade
+    public float rotationSpeed = 100.0f;    // Para fazer a rotação
+    public Animator animator;               // Para fazer a animação
+    public bool Walk = false;         // Indicador de caminhada
 
     void Start()
     {
         animator = GetComponent<Animator>();
     }
 
+    // Update is called once per frame
     void Update()
     {
         float move = Input.GetAxis("Vertical");
-        float turn = Input.GetAxis("Horizontal");
+        float turn = Input.GetAxis("Horizontal");               // Para girar, rotacionar
 
-        if (move != 0)
+        if(move != 0)
+        {
+            animator.SetBool("Run", true);
+            Vector3 movement = transform.forward * move * speed * Time.deltaTime;
+            transform.position += movement;
+        }
+        else{
+            animator.SetBool("Run", false);
+        }
+        if (turn !=0)
+        {
+            float rotation = turn * rotationSpeed * Time.deltaTime;
+            transform.Rotate(0,rotation,0);
+        }
+         if (Walk)
         {
             animator.SetBool("Walk", true);
-            Vector3 movement = transform.forward * move * speed * Time.deltaTime;
+            Vector3 movement = transform.forward * speed * Time.deltaTime;
             transform.position += movement;
         }
         else
         {
             animator.SetBool("Walk", false);
+    }
+    if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ToggleWalk();
         }
 
-        if (turn != 0)
-        {
-            float rotation = turn * rotationSpeed * Time.deltaTime;
-            transform.Rotate(0, rotation, 0);
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Attack();
-        }
     }
 
-    void Attack()
+    void ToggleWalk()
     {
-
-        animator.SetTrigger("Attack");
-
-    }
+        Walk = !Walk;
+        animator.SetBool("Walk", Walk);
+    }    
 }
